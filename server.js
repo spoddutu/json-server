23c169489ee91e06d0cc5712bcfe7e4f21710dc4
spoddutu <santhosh.podduturi@gmail.com>
2016-03-08 02:53:33 -0500

    Added new files

---
+++ server.js
@@ -0,0 +1,12 @@
+var jsonServer = require('json-server');
+
+// Returns an express server
+var server = jsonServer.create();
+
+// Set default middlewares (logger, static, cors and no-cache) 
+server.use(jsonServer.defaults());
+
+var router = jsonServer.router('db.json');
+server.use(router);
+
+server.listen(3005);
